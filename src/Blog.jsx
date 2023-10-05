cd4c872289ede0a8ca92502b35910e0b7364247b
Aryan <aryan09raghuvanshi@gmail.com>
2023-10-05 22:53:07 +0530

    Build error fixed

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -10,7 +10,7 @@ const Blog = () => {
   return (
     <div className='gpt3__blog section__padding' id='blog'>
      <div className='gpt3__blog-heading'>
-      <h1 className='gradient-text'></h1>
+      <h1 className='gradient-text'>Temp</h1>
      </div>
      <div className='gpt3__blog-container'>
       <div className='gpt3__blog-container_groupA'>
